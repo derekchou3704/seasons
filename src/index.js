a82446d74be701ba4173c1c041f9aadf0f52451d
DerekChou <derekchou3704@gmail.com>
2021-05-11 19:16:20 +0800

    Adding SeasonConfig to determine the season according to latitude with geolocation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay'
+
+
+class App extends React.Component {
+    // Everytime an instance is created, constructor will be called
+    // Not the only method but common in React to initialize Class
+    constructor(props) {
+        super(props);
+
+        //This is the ONLY TIME we asign this.state directly
+        this.state = { latitude: null, errorMessage: '' };
+    }
+
+    // state = { latitude: null, errorMessage: '' }; //Works the same as above
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ latitude: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    // componentDidUpdate() {
+    //     console.log('My component did rerendered!')
+    // }
+
+    // In react render must be defined!!
+    render() {
+        if (this.state.errorMessage && !this.state.latitude) {
+            return(
+                <div>                    
+                    Error: {this.state.errorMessage}
+                </div>
+            );
+        } 
+        if (!this.state.errorMessage && this.state.latitude) {
+            return <SeasonDisplay lat={this.state.latitude} />
+        } 
+        
+        return(
+            <div>
+                Loading...
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App/>,
+    document.querySelector('#root')
+)
